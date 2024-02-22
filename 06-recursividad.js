eb1665cb5735412fddf37317df9439f9be9d781b
facundo enriquez <facundo.tomas.enriquez@gmail.com>
2024-02-21 22:44:45 -0300

    pilas y colas

--- 06-recursividad.js
+++ 06-recursividad.js
@@ -1,19 +1,32 @@
 let i = 0;
 function imprimir_numeros() {
-    if (i != 100) {
-        i += 1;
-        console.log(i);
-        imprimir_numeros();
-    }
+  if (i != 100) {
+    i += 1;
+    console.log(i);
+    imprimir_numeros();
+  }
 }
 
 function factorial(num) {
-    if (num === 0 || num === 1) {
-        return 1;
-    } else {
-        return num * factorial(num - 1);
-    }
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+let arr = [0, 1];
+function fibonacci(pos) {
+  if (arr[pos] == undefined) {
+    last_position = arr.length - 1;
+    arr.push(arr[last_position - 1] + arr[last_position]);
+    console.log(arr)
+    return fibonacci(pos);
+  } else {
+    return arr[pos]
+  }
 }
 
 // imprimir_numeros();
-console.log(factorial(4));
+// console.log(factorial(4));
+console.log(fibonacci(10));
---
+++ 07-pilas_colas.js
@@ -0,0 +1,5 @@
+function pilas(array) {
+    return array
+}
+
+console.log(pilas([1, 2, 3, 4, 5]));
